207d058ac249309218a9e223fa0b1397f7173ce5
Ben Hagag <benbenhagag@google.com>
2020-10-27 11:04:06 +0200

    initalize My React project For Learning  - How jsx works behind the scene

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am a React app</h1>
+      </div>
+    );
+
+
+
+
+    // ---- Behind the scene code of the JSX ----- 
+
+    /*
+      React JSX - using HTML in js file.
+      Behind the scene - what it does it uses in a method  - 
+      React.createElement('div',{className: App}, "The text I want to represent in the div");
+      So JSX saves for us code to use and in the end it compiles to JS. 
+      without JSX we would use React.createElement for each element we want to create nested in each other.
+    */
+    
+    // Code example:
+    
+    // return React.createElement(
+    //         'div', 
+    //         {className: 'App'}, 
+    //         React.createElement(
+    //           'h1',
+    //           null,
+    //           'Hi, I am a React App'
+
+    //         )
+    //       );
+
+    // ---- END Behind the scene code of the JSX ----- 
+  }
+}
+
+export default App;
