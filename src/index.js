e817ca164f775e619c32cc34fd633b9c57d01153
Ben Hagag <benbenhagag@google.com>
2020-11-08 20:29:26 +0200

    How react updates the DOM

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// How React updates the DOM - https://www.codecademy.com/articles/react-virtual-dom#:~:text=A%20virtual%20DOM%20object%20has,because%20nothing%20gets%20drawn%20onscreen.
+
 ReactDOM.render(<App appTitle="Learn React"/>, document.getElementById('root'));
 registerServiceWorker();
